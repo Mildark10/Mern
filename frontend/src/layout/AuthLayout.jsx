26d5714ae5e2444e2ecc76571ee0dd6b3c5206ee
Avalos Pucyura <mmavalos@indracompany.com>
2024-01-08 12:07:06 -0500

    comenzando con backend react

---
+++ frontend/src/layout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line no-unused-vars
+import { Outlet } from "react-router-dom"
+export const AuthLayout = () => {
+  return (
+    <>
+        <h1>Desde lauput</h1>
+        <Outlet/>
+    </>
+  )
+}
---
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -0,0 +1,11 @@
+
+export const ConfirmarCuenta = () => {
+    return (
+      <>
+          <h1>Desde ConfirmarCuenta</h1>
+        
+      </>
+    )
+  }
+  
+  
